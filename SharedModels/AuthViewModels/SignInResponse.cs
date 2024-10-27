96ca851f3d405aff94b51422b416bcd770b0f7be
Raatbek <>
2024-10-28 00:32:42 +0600

    add models

---
+++ SharedModels/AuthViewModels/SignInResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.AuthViewModels
+{
+    public class SignInResponse
+    {
+        public bool IsSuccess { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+        public string Username { get; set; }
+        public string JwtToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime JwtExpires { get; set; }
+    }
+}
