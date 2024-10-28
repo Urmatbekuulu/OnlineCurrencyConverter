1d0fe28d1cbae03ee529aeb5d723894cdb89ec6f
Raatbek <>
2024-10-28 12:16:46 +0600

    add startup services file

---
+++ Backend/Business Logic Layer/StartupSetup.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Data_Access_Layer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Business_Logic_Layer
+{
+    public static class StartupSetup
+    {
+        public static void AddBusinessAccessLayerServices(this IServiceCollection services, IConfiguration configuration)
+        {
+          
+        }
+    }
+}
---
+++ Backend/Data Access Layer/StartupSetup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data_Access_Layer
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContexts(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+        }
+
+        public static void AddDataAccessLayerServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
