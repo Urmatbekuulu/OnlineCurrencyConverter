f3cb04531b0d7bdd642b094a2915dc58bac449db
Raatbek <>
2024-10-29 15:24:47 +0600

    last commit

---
+++ Frontend/Pages/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿using Frontend.Services;
+using Microsoft.AspNetCore.Components;
+using SharedModels.CurrenciesViewModel;
+using System.Net.Http.Json;
+
+namespace Frontend.Pages
+{
+    public partial class Home
+    {
+        [Inject] private IHttpService HttpService { get; set; }
+
+        private List<CurrencyViewModel> Currencies = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Currencies = await GetTodaysCurrencies();
+        }
+        public async Task<List<CurrencyViewModel>> GetTodaysCurrencies()
+        {
+            var response = await HttpService.HttpGetAsync($"api/currencies");
+            var result = await response.Content.ReadFromJsonAsync<List<CurrencyViewModel>>();
+            if (result is null) return default;
+            return result;
+        }
+    }
+}
